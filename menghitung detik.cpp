3d664694e8f2a161ea87a02ab6d1fc4dfcbe7b8d
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-11-09 16:12:09 +0700

    code

--- menghitung detik.cpp
+++ menghitung detik.cpp
@@ -1,24 +1,20 @@
 #include <iostream>
 using namespace std;
-
-
-int main() {
+int main() 
+{
    int n,jam,menit;
-    cin >> n;
+   cin >> n;
     
     if(n>3600){
     	jam=n/3600 ;
-    	menit=(n-jam*3600)/60 ;
-		
-		cout << jam << " jam " << menit << " menit " << n-jam*3600-menit*60 << " detik " << endl;
-	}
-    
+    	menit=(n-jam*3600)/60 ;	
+	cout << jam << " jam " << menit << " menit " << n-jam*3600-menit*60 << " detik " << endl;
+    }
     else{
-	
         jam=n/3600 ;
-        menit=n/60 ;
-	    
+        menit=n/60 ;  
         cout << jam << " jam " << menit << " menit " << n-menit*60 << " detik " << endl;
     }
-	return 0;
+    
+    return 0;
 }
