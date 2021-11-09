e2fef51fbe352bbb2242e9a195dc59722ce0c146
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-11-09 16:09:20 +0700

    Add files via upload

---
+++ menghitung detik.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+   int n,jam,menit;
+    cin >> n;
+    
+    if(n>3600){
+    	jam=n/3600 ;
+    	menit=(n-jam*3600)/60 ;
+		
+		cout << jam << " jam " << menit << " menit " << n-jam*3600-menit*60 << " detik " << endl;
+	}
+    
+    else{
+	
+        jam=n/3600 ;
+        menit=n/60 ;
+	    
+        cout << jam << " jam " << menit << " menit " << n-menit*60 << " detik " << endl;
+    }
+	return 0;
+}
