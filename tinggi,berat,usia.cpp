5e7a91832edd0ce5ac3318803efac8bfe25ef024
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-11-09 16:12:47 +0700

    code

--- tinggi,berat,usia.cpp
+++ tinggi,berat,usia.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-
-
-int main() {
+int main() 
+{
     int u;
     float t,b;
     cin >> t; cin >> b; cin >> u; 
+    
     if (t>170 && b>55 && u>=15){
         cout << "Lolos" << endl;
     }
     else {
         cout << "Tidak Lolos" << endl;
     }
+    
     return 0;
 }
