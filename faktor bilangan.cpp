60607cdf6ad269bc42c5d58c52be358c8ccdf906
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-12-23 19:47:53 +0700

    Update faktor bilangan.cpp

--- faktor bilangan.cpp
+++ faktor bilangan.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 using namespace std;
+
 int main(){
+	
 	int n,arr[n],jumlah=0;
 	cin >>n;
+	
 	for(int i=n;i>0;i--){
-		if(n%i==0){
-			jumlah+=1;
-		}		
+	   if(n%i==0){
+    	      jumlah+=1;
+	   }		
 	}
+	
 	if(jumlah==5){
-				cout << "YES";
-			}else if(jumlah!=5){
-				cout << "NO";
-			
-		}
+	   cout << "YES";
+	}
+	else if(jumlah!=5){
+	   cout << "NO";
+	}
 }
