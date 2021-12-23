a5acedcdd49bc92391598f9318f702619ec2a859
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-12-23 19:49:49 +0700

    code

--- konversi biner.cpp
+++ konversi biner.cpp
@@ -7,13 +7,13 @@ int main(){
 	cin >> a;
 	
 	if(a>=1&&a<=10000){
-		for(i=0;a>0;i++){
-			arr[i]=a%2;
-			a=a/2;
-		}
-		for(i=i-1 ;i>=0 ;i--){
+	   for(i=0;a>0;i++){
+		arr[i]=a%2;
+		a=a/2;
+	   }
+	   for(i=i-1 ;i>=0 ;i--){
     		cout << arr[i];
-  		}
-  		cout << endl;		
+  	   }
+  	   cout << endl;		
 	} 
 }
