9917ed24217466e02a563642df323fd101914af7
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-12-23 19:46:39 +0700

    Add files via upload

---
+++ faktor bilangan.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int n,arr[n],jumlah=0;
+	cin >>n;
+	for(int i=n;i>0;i--){
+		if(n%i==0){
+			jumlah+=1;
+		}		
+	}
+	if(jumlah==5){
+				cout << "YES";
+			}else if(jumlah!=5){
+				cout << "NO";
+			
+		}
+}
