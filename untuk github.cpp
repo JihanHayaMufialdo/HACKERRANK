dc4e6b6ee53335c832d0b61e389fc993dce29808
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-12-23 19:45:53 +0700

    Update untuk github.cpp

--- untuk github.cpp
+++ untuk github.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 using namespace std;
+
 int main(){
+	
 	int a,arr[10000],i;
 	cin >> a;
+	
 	if(a>=1&&a<=10000){
 		for(i=0;a>0;i++){
 			arr[i]=a%2;
 			a=a/2;
 		}
-		for(i=i-1 ;i>=0 ;i--) {
+		for(i=i-1 ;i>=0 ;i--){
     		cout << arr[i];
   		}
-  		cout << endl;	
-			
+  		cout << endl;		
 	} 
 }
