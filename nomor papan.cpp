366f8f7374ee727ded4c4b2407a6c8aa95051e75
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-12-27 19:13:47 +0700

    Update nomor papan.cpp

--- nomor papan.cpp
+++ nomor papan.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 int main(){
 	
+	int n;
+	cin >> n;
+	
+	int arr[n];
+	
+	if(n>=1&&n<=1000){
+	for(int i=0; i<n; i++){
+		cin  >> arr[i];		
+	}
+}
+		for(int i=n-1;i>=0;i--){
+			cout << arr[i] << " ";
+		}
 		
 }
